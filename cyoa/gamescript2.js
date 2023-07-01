796ade2d4a6e4b8c5397826762ba54fa3c9b82e0
EvanDHeller <134806751+EvanDHeller@users.noreply.github.com>
2023-07-01 18:41:53 -0400

    Update gamescript2.js

--- cyoa/gamescript2.js
+++ cyoa/gamescript2.js
@@ -124,7 +124,19 @@ function saveName() {
   saveButton.textContent = "Saved";
   saveButton.disabled = true;
 
-  story.scenes.startGame.displayNextMessage();
+  hideNameInput();
+
+  // Check if the current scene is the "startGame" scene
+  if (story.currentScene === "startGame") {
+    // Check if this is the last message in the "startGame" scene
+    if (story.messageIndex === story.messages.length - 1) {
+      // Progress to the next scene
+      story.scenes.scene2.scene2();
+    } else {
+      // Display the next message in the "startGame" scene
+      story.scenes.startGame.displayNextMessage();
+    }
+  }
 }
 
 function resetNameInput() {
