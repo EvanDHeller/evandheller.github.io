0711c05c5505214ba9b5ac073a24137bc3d18018
EvanDHeller <134806751+EvanDHeller@users.noreply.github.com>
2023-07-02 06:16:39 -0400

    Update gamescript2.js

--- cyoa/gamescript2.js
+++ cyoa/gamescript2.js
@@ -157,13 +157,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
    endGameNegative: {
   endGameNegative: function() {
-    showOptions([]);
-    showMessage("Sorry, " + story.playerName + ", yours was not a story with a happy ending. Better luck next time!");
-    var playAgainButton = document.getElementById("play-again-button");
-    playAgainButton.style.display = "block"; // Show the "Play Again" button
-    playAgainButton.textContent = "Try Again"; // Change the text of the button
-    story.currentScene = "startGame";
+  showOptions([]);
+  showMessage("Sorry, " + story.playerName + ", yours was not a story with a happy ending. Better luck next time!");
+  var playAgainButton = document.getElementById("play-again-button");
+  playAgainButton.style.display = "block"; // Show the "Play Again" button
+  playAgainButton.textContent = "Try Again"; // Change the text of the button
+
+  playAgainButton.addEventListener("click", function() {
+    story.scenes.startGame.startGame(); // Call the startGame function to restart the game
+  });
+
+  story.currentScene = "startGame";
 }
+
 }
 }
   };
