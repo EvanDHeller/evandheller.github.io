41d98c090fb78ebd664d0965fe1504e5742f93fc
EvanDHeller <134806751+EvanDHeller@users.noreply.github.com>
2023-07-02 06:07:11 -0400

    Update gamescript2.js

--- cyoa/gamescript2.js
+++ cyoa/gamescript2.js
@@ -157,6 +157,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
    endGameNegative: {
   endGameNegative: function() {
+    showOptions([]);
     showMessage("Sorry, " + story.playerName + ", yours was not a story with a happy ending. Better luck next time!");
     var playAgainButton = document.getElementById("play-again-button");
     playAgainButton.style.display = "block"; // Show the "Play Again" button
