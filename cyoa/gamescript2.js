7d58e33e0e1eeaa5d4cc488dd528627caf93586d
EvanDHeller <134806751+EvanDHeller@users.noreply.github.com>
2023-07-01 23:22:07 -0400

    Update gamescript2.js

--- cyoa/gamescript2.js
+++ cyoa/gamescript2.js
@@ -1,105 +1,122 @@
 document.addEventListener("DOMContentLoaded", function() {
-const story = {
-  currentScene: "startGame",
-  playerName: null,
-  messages: [],
-  messageIndex: 0,
-  scenes: {
-    startGame: {
-      startGame: function () {
-        this.playerName = null;
-        this.messages = [
-          { text: "You have just woken up, you don't know where you are, or how you got there.", image: "" },
-          { text: "You don't even remember your name!", image: "" },
-          { text: "As you yawn and rub your eyes, you realize even a temporary name may be helpful...", image: "" }
-        ];
-        this.messageIndex = 0;
-        this.displayNextMessage();
-      },
-      displayNextMessage: function () {
-        if (this.messageIndex < this.messages.length) {
-          showMessage(
-            this.messages[this.messageIndex].text,
-            this.messages[this.messageIndex].image
-          );
+  const story = {
+    currentScene: "startGame",
+    playerName: null,
+    messages: [],
+    messageIndex: 0,
+    scenes: {
+      startGame: {
+        startGame: function () {
+          this.playerName = null;
+          this.messages = [
+            { text: "You have just woken up, you don't know where you are, or how you got there.", image: "" },
+            { text: "You don't even remember your name!", image: "" },
+            { text: "As you yawn and rub your eyes, you realize even a temporary name may be helpful...", image: "" }
+          ];
+          this.messageIndex = 0;
+          this.displayNextMessage();
+        },
+        displayNextMessage: function () {
+          if (this.messageIndex < this.messages.length) {
+            showMessage(
+              this.messages[this.messageIndex].text,
+              this.messages[this.messageIndex].image
+            );
 
-          if (this.messageIndex === this.messages.length - 1) {
-            showNameInput();
-            hideContinueButton();
-          } else {
-            showContinueButton();
+            if (this.messageIndex === this.messages.length - 1) {
+              showNameInput();
+              hideContinueButton();
+            } else {
+              showContinueButton();
+            }
+
+            this.messageIndex++;
           }
 
-          this.messageIndex++;
+          if (this.currentScene === "startGame" && this.messageIndex === 1) {
+            // Hide the start button after the first message
+            hideStartButton();
+          }
         }
-
-        if (this.currentScene === "startGame" && this.messageIndex === 1) {
-          // Hide the start button after the first message
-          hideStartButton();
+      },
+      scene2: {
+        scene2_1: function () {
+          showMessage("Thank you, " + story.playerName + ", your adventure begins now!");
+          showContinueButton();
+          const continueButton = document.getElementById("continue-button");
+          continueButton.addEventListener("click", story.scenes.scene2.scene2_2);
+        },
+        scene2_2: function () {
+          showMessage("As you look around and take in your surroundings, you find yourself in a small wooded enclave.");
+          showContinueButton();
+          const continueButton = document.getElementById("continue-button");
+          continueButton.addEventListener("click", story.scenes.scene2.scene2_3);
+        },
+        scene2_3: function () {
+          showMessage("In the center of the enclave, you notice a cleanly cut tree trunk with a large locked chest atop it.");
+          showOptions([
+            { text: "Kick at the rusted lock, hoping it will open.", action: story.scenes.scene3.scene3_1 },
+            { text: "Look around to see if there is anything you can use to open the chest.", action: story.scenes.scene3.scene3_2 },
+            { text: "You know, this is someone's property, I should probably leave it alone.", action: story.scenes.scene3.scene3_3 }
+          ]);
         }
-      }
-    },
-    scene2: {
-      scene2_1: function () {
-        showMessage("Thank you, " + story.playerName + ", your adventure begins now!");
-        showContinueButton();
-        const continueButton = document.getElementById("continue-button");
-        continueButton.addEventListener("click", story.scenes.scene2.scene2_2);
       },
-      scene2_2: function () {
-        showMessage("As you look around and take in your surroundings, you find yourself in a small wooded enclave.");
-        showContinueButton();
-        const continueButton = document.getElementById("continue-button");
-        continueButton.addEventListener("click", story.scenes.scene2.scene2_3);
+      scene3: {
+        scene3_1: function () {
+          showMessage("You kick at the rusted lock, but it doesn't budge. It seems sturdy.");
+          story.currentScene = "scene4";
+          showOptions([
+            { text: "Go back to the previous scene.", action: story.scenes.scene2.scene2_3 },
+            { text: "Look around to see if there is anything you can use to open the chest.", action: story.scenes.scene3.scene3_2 },
+            { text: "You know, this is someone's property, I should probably leave it alone.", action: story.scenes.scene3.scene3_3 }
+          ]);
+        },
+        scene3_2: function () {
+          showMessage("You search the area and find a crowbar hidden behind a tree. It might come in handy.");
+          story.currentScene = "scene4";
+          showOptions([
+            { text: "Go back to the previous scene.", action: story.scenes.scene2.scene2_3 },
+            { text: "Kick at the rusted lock, hoping it will open.", action: story.scenes.scene3.scene3_1 },
+            { text: "You know, this is someone's property, I should probably leave it alone.", action: story.scenes.scene3.scene3_3 }
+          ]);
+        },
+        scene3_3: function () {
+          showMessage("You decide it's best not to meddle with someone else's property and leave the chest alone.");
+          story.currentScene = "scene4";
+          showOptions([
+            { text: "Go back to the previous scene.", action: story.scenes.scene2.scene2_3 },
+            { text: "Kick at the rusted lock, hoping it will open.", action: story.scenes.scene3.scene3_1 },
+            { text: "Look around to see if there is anything you can use to open the chest.", action: story.scenes.scene3.scene3_2 }
+          ]);
+        }
       },
-      scene2_3: function () {
-        showMessage("In the center of the enclave, you notice a cleanly cut tree trunk with a large locked chest atop it.");
-        showOptions([
-          { text: "Kick at the rusted lock, hoping it will open.", action: story.scenes.scene3.scene3_1 },
-          { text: "Look around to see if there is anything you can use to open the chest.", action: story.scenes.scene3.scene3_2 },
-          { text: "You know, this is someone's property, I should probably leave it alone.", action: story.scenes.scene3.scene3_3 }
-        ]);
-      }
-    },
-    scene3: {
-      scene3_1: function () {
-        showMessage("You kick at the rusted lock, but it doesn't budge. It seems sturdy.");
-        story.currentScene = "scene4";
-        showOptions(story.scenes.scene4.options);
+      scene4: {
+        scene4_1: function () {
+          showMessage("You use the crowbar to pry open the chest. Inside, you find a map and a key.");
+          showMessage("The map reveals the location of a hidden treasure nearby. Excited, you decide to go searching for it.");
+          story.currentScene = "endGame";
+          showEndMessage();
+        },
+        scene4_2: function () {
+          showMessage("You decide it's best not to meddle with someone else's property and leave the chest alone.");
+          story.currentScene = "endGame";
+          showEndMessage();
+        },
+        scene4_3: function () {
+          showMessage("You decide it's best not to meddle with someone else's property and leave the chest alone.");
+          story.currentScene = "endGame";
+          showEndMessage();
+        }
       },
-      scene3_2: function () {
-        showMessage("You search the area and find a crowbar hidden behind a tree. It might come in handy.");
-        story.currentScene = "scene4";
-        showOptions(story.scenes.scene4.options);
-      },
-      scene3_3: function () {
-        showMessage("You decide it's best not to meddle with someone else's property and leave the chest alone.");
-        story.currentScene = "endGame";
-        showEndMessage();
-      }
-    },
-    scene4: {
-      scene4_1: function () {
-        showMessage("You use the crowbar to pry open the chest. Inside, you find a map and a key.");
-        showMessage("The map reveals the location of a hidden treasure nearby. Excited, you decide to go searching for it.");
-        story.currentScene = "endGame";
-        showEndMessage();
-      },
-      scene4_2: function () {
-        showMessage("You decide it's best not to meddle with someone else's property and leave the chest alone.");
-        story.currentScene = "endGame";
-        showEndMessage();
-      }
-    },
-    endGame: {
-      endGame: function () {
-        showMessage("Congratulations, you have reached the end of the game!");
-        showOptions([{ text: "Play Again", action: story.scenes.startGame.startGame }]);
-        story.currentScene = "startGame";
+      endGame: {
+        endGame: function () {
+          showMessage("Congratulations, you have reached the end of the game!");
+          showOptions([{ text: "Play Again", action: story.scenes.startGame.startGame }]);
+          story.currentScene = "startGame";
+        }
       }
     }
-  }
-};
+  };
 
 function showMessage(text, image = "") {
   const messageElement = document.getElementById("message");
