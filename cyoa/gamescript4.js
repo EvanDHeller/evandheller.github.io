5439c4ba21f8ea2f1878ea9dfef5adaa02469e2d
EvanDHeller <134806751+EvanDHeller@users.noreply.github.com>
2023-07-03 06:12:47 -0400

    Update gamescript4.js

--- cyoa/gamescript4.js
+++ cyoa/gamescript4.js
@@ -152,49 +152,23 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
             },
             endGameNegative: {
-                endGameNegative: function () {
+    endGameNegative: function () {
         if (story.currentScene === "endGameNegative") {
-                    showMessage("Sorry, " + story.playerName + ", yours was not a story with a happy ending. Better luck next time!");
-                    showOptions([]);
-                    playAgainButton();
-                    playAgainButton.textContent = "Try Again";
-                    playAgainButton.style.display = "block";
+            showMessage("Sorry, " + story.playerName + ", yours was not a story with a happy ending. Better luck next time!");
+            showOptions([]);
+            playAgainButton();
+            playAgainButton.textContent = "Try Again";
+            playAgainButton.style.display = "block";
         }
-                    
-                    
-                   if (gameEnded) {
-                     return; // Return early if the game has already ended
-                   }
 
-                   gameEnded = true; // Set the gameEnded variable to true to indicate that the game has ended 
-
-                    
-                    //showMessage("Sorry, " + story.playerName + ", yours was not a story with a happy ending. Better luck next time!");
-                    
-                  /*  var playAgainButton = document.getElementById("play-again-button"); */
-                 //   playAgainButton();
-                     // Show the "Try Again" button
-                 /*   playAgainButton.textContent = "Try Again"; // Change the text of the button
-
-                    if (story.messageIndex === 7 || story.messageIndex === 8) {
-                        playAgainButton.style.display = "none"; // Hide the "Try Again" button for the specific message indexes
-                    }
-
-                    playAgainButton.addEventListener("click", function () {
-                        hideTryAgainButton();
-                        resetNameInput();
-                        gameEnded = false; // Reset the gameEnded variable
-                        story.currentScene = "startGame";
-                        story.scenes.startGame(); // Call the startGame function to restart the game
-                    });
-                     hideTryAgainButton();
-                        resetNameInput();
-                    hideContinueButton();
-                    showTryAgainButton() */
-                }
-            }
-
+        if (gameEnded) {
+            return; // Return early if the game has already ended
         }
+
+        gameEnded = true; // Set the gameEnded variable to true to indicate that the game has ended
+    }
+}
+
     };
 //**************************************GLOBAL FUNCTIONS*******************************************************************************************
 
