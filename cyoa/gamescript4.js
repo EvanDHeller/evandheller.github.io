6c06356d93d9ff88d3009b1ebb6e76fca18143af
EvanDHeller <134806751+EvanDHeller@users.noreply.github.com>
2023-07-03 22:31:30 -0400

    Update gamescript4.js

--- cyoa/gamescript4.js
+++ cyoa/gamescript4.js
@@ -162,6 +162,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     showMessage("Sorry, " + story.playerName + ", yours was not a story with a happy ending. Better luck next time!");
     
                     const startGameListener = function() {
+                        hideContinueButton();
                         story.scenes.startGame();
                         continueButton.removeEventListener("click", startGameListener);
                                 };
